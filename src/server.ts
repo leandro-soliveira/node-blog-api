969c957e31fcf60b9d825d355cb10ffcc4290e7b
Leandro S Oliveira <leandrosoliveira10@outlook.com>
2026-02-08 15:45:04 -0300

    chore: initial project setup

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const server = express();
+
+server.use(cors());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.static('public'));
+
+server.get('/ping', (request, response) => {
+    response.json({ ping: true })
+});
+
+server.listen(4444, () => {
+    console.log('B7Blog backend porta http://localhost:4444')
+})

a3390ebff958291110b030fb7b12bf5f6986b867
Leandro S Oliveira <leandrosoliveira10@outlook.com>
2026-02-16 23:05:29 -0300

    feat: implement admin post creation with service layer and image upload

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -1,21 +1,85 @@
-import { RequestHandler } from "express";
+import { RequestHandler, Response } from "express";
+import { ExtenedRequest } from "../types/extended-request";
+import z from "zod";
+import { createPost, createPostSlug, handleCover } from "../services/post";
+import { getUserById } from "../services/user";
+import { coverToUrl } from "../utils/cover-to-url";
 
-export const addPost: RequestHandler = async (request, response) => {
-    return response.status(501).json({ message: "Not implemented yet"});
+export const addPost = async (request: ExtenedRequest, response: Response) => {
+    if (!request.user) return;
+
+    const schema = z.object({
+        title: z
+            .string()
+            .trim()
+            .min(5, "Título deve ter pelo menos 5 caracteres")
+            .max(120, "Título muito longo"),
+        tags: z.string().trim(),
+        body: z.string().trim().min(10, "O conteúdo deve ter pelo menos 10 caraceteres")
+    });
+
+    const data = schema.safeParse(request.body);
+    if (!data.success) {
+        const errors = data.error.issues.map(issue => ({
+            field: issue.path[0],
+            message: issue.message
+        }));
+        return response.status(400).json({ errors });
+    }
+
+    if (!request.file) {
+        response.status(400).json({ error: 'Imagem é obrigatória' });
+        return;
+    }
+
+    // --Lidar com o arquivo
+    const coverName = await handleCover(request.file);
+    if (!coverName) {
+        response.status(400).json({ error: 'Imagem não permitida/enviada' });
+        return;
+    }
+    // --Criar o slug do post
+    const slug = await createPostSlug(data.data.title);
+
+    // --Criar o post
+    const newPost = await createPost({
+        authorId: request.user.id,
+        slug,
+        title: data.data.title,
+        tags: data.data.tags,
+        body: data.data.body,
+        cover: coverName
+    })
+
+    // --Pegar informações do autor
+    const author = await getUserById(newPost.authorId);
+
+    // --Faser retorno segundo o plano
+    response.status(201).json({
+        post: {
+            id: newPost.id,
+            slud: newPost.slug,
+            title: newPost.title,
+            createdAt: newPost.createdAt,
+            cover: coverToUrl(newPost.cover),
+            tags: newPost.tags,
+            authorName: author?.name
+        }
+    })
 };
 
 export const getPosts: RequestHandler = async (request, response) => {
-    return response.status(501).json({ message: "Not implemented yet"});
+    return response.status(501).json({ message: "Not implemented yet" });
 };
 
 export const getPost: RequestHandler = async (request, response) => {
-    return response.status(501).json({ message: "Not implemented yet"});
+    return response.status(501).json({ message: "Not implemented yet" });
 };
 
 export const editPost: RequestHandler = async (request, response) => {
-    return response.status(501).json({ message: "Not implemented yet"});
+    return response.status(501).json({ message: "Not implemented yet" });
 };
 
 export const removePost: RequestHandler = async (request, response) => {
-    return response.status(501).json({ message: "Not implemented yet"});
+    return response.status(501).json({ message: "Not implemented yet" });
 };
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import * as adminController from '../controllers/admin';
+import { privateRoute } from '../middlewares/private-route';
+import { upload } from '../libs/multer';
 
 export const adminRoutes = Router();
 
-adminRoutes.post('/posts', adminController.addPost); // ainda não implementado
+adminRoutes.post('/posts', privateRoute, upload.single('cover'), adminController.addPost);
 adminRoutes.get('/posts', adminController.getPosts); // ainda não implementado
 adminRoutes.get('/posts/:slug', adminController.getPost); // ainda não implementado
 adminRoutes.put('/posts/:slug', adminController.editPost); // ainda não implementado
